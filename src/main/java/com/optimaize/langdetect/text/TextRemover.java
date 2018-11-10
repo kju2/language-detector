2806a0f5bdfaef391431f4997e09607530ee69cc
kju2 <1471302+kju2@users.noreply.github.com>
2018-11-10 16:58:54 +0100

    Add documentation.

--- src/main/java/com/optimaize/langdetect/text/TextRemover.java
+++ src/main/java/com/optimaize/langdetect/text/TextRemover.java
@@ -2,6 +2,18 @@ package com.optimaize.langdetect.text;
 
 import java.util.function.UnaryOperator;
 
+/**
+ * Takes a {@link StringBuilder} and removes parts of the content. This will in most cases modify
+ * the given {@link StringBuilder}, but it is also possible to return a new {@link StringBuilder}
+ * instance.
+ * <p>
+ * TextRemovers are intended to be chained. To not impact the performance by creating and throwing
+ * away strings all the time, we use a {@link StringBuilder} .
+ */
+@FunctionalInterface
 public interface TextRemover extends UnaryOperator<StringBuilder> {
-
+	/**
+	 * No changes to the {@link UnaryOperator} necessary. We just want to make clear what the
+	 * methods do that implement this interface.
+	 */
 }
--- src/main/java/com/optimaize/langdetect/text/TextRemovers.java
+++ src/main/java/com/optimaize/langdetect/text/TextRemovers.java
@@ -3,8 +3,23 @@ package com.optimaize.langdetect.text;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class TextRemovers {
+
+	/**
+	 * Removes all URLs. Modifies the given {@link StringBuilder} instance.
+	 * <p/>
+	 * <b>WARNING</b>: Doesn't recognize URLs with internationalized domain names.
+	 */
 	public static final TextRemover url = createRegexRemover("https?://[-_.?&~;+=/#0-9A-Za-z]+");
+
+	/**
+	 * Removes all emails. Modifies the given {@link StringBuilder} instance.
+	 * <p/>
+	 * <b>WARNING</b>: Doesn't recognize emails with internationalized domain names.
+	 */
 	public static final TextRemover email = createRegexRemover("[-_.0-9A-Za-z]+@[-_0-9A-Za-z]+[-_.0-9A-Za-z]+");
 
 	public static TextRemover createRegexRemover(Pattern pattern) {
